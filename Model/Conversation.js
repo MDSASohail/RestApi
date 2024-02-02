515577b454665c675d013f6f2198195d724891d3
Sohail <thisemailitakenbysohail@gmail.com>
2024-02-02 18:39:24 +0530

    Final

--- Model/Conversation.js
+++ Model/Conversation.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const conversationSchema = new Schema({
-    members: { type: Array,unique:true }
+    members: { type: Array, unique: true, sparse: true }
 }, { timestamps: true });
-
-// Validate uniqueness within the 'members' array for each document
-conversationSchema.path('members').validate(function (value) {
-    // Assuming 'value' is the members array
-    return Array.isArray(value) && new Set(value).size === value.length;
-}, 'Members array must have unique elements');
 
 module.exports = mongoose.model('Conversation', conversationSchema);
